210233ec50663a3c39555849480d69441f57b266
samorafi <sebasmora172522@gmail.com>
2024-07-01 11:10:00 -0600

    Initial commit

---
+++ src/main/java/main/Proyecto_SC_302.java
@@ -0,0 +1,9 @@
+
+package main;
+
+public class Proyecto_SC_302 {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto Estructuras de Datos, Primer Avance");
+    }
+}
